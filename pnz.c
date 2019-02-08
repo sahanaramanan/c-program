b1c191aa4f9b16cb912d01b47bca2601ffb8a220
sahanaramanan <47449689+sahanaramanan@users.noreply.github.com>
2019-02-08 18:15:29 +0530

    Update and rename vowle.c to pnz.c

---
+++ pnz.c
@@ -0,0 +1,16 @@
+#incluse<stdio.h>
+int main()
+{
+	int n;
+	scanf("%d",&n);
+	if(n>0)
+	{
+		printf("positive");
+		else if(n=0)
+		{
+			printf("zero");
+			else
+			printf("negative");
+		}
+	}
+}
