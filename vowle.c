cce7eee0b4747213c3ce70f9e0d764133b43c601
sahanaramanan <47449689+sahanaramanan@users.noreply.github.com>
2019-02-08 18:05:21 +0530

    Update and rename even.c to vowle.c

---
+++ vowle.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	char ch;
+	scanf("%d",&n);
+	if(ch=='a' || ch=='e' || ch=='i' ||ch=='o' ||ch='u')
+	{
+		printf("%c vowle",ch);
+		else
+		{
+			printf("%c not vowle",ch);
+		}
+		
+	}
+	
+}
